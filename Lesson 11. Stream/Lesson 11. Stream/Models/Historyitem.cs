6fe0ee15870a431504b32d1555086b19854ab621
Murad-Ahmedov7 <mrdhmdv04@gmail.com>
2024-07-10 01:27:01 +0400

    Historyitem.cs

--- Lesson 11. Stream/Lesson 11. Stream/Models/Historyitem.cs
+++ Lesson 11. Stream/Lesson 11. Stream/Models/Historyitem.cs
@@ -7,10 +7,4 @@ namespace Lesson_11._Stream.Models
 }
 
 
-/////// recorda murad dedi parametrik(onun kimi bir sey var primary o isleyir ve costructor-a ehtiyac olmur) isleyir ve property ile uzun muddetli istifade olur amma obyektde ise kisa muddetli istifade olunur.
-
-
-
-
-// user hissesi propertyler ve field ferqi sorus
 
